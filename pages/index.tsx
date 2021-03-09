181b657238c044a50de65b601ca61e43ad832846
March Sabino <marchsabino@gmail.com>
2021-03-09 01:20:11 -0500

    Use husky to lint before committing code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head"
+import Head from "next/head";
 
 /**
  * The Homepage Component for Honey Pot Glen.
- * 
+ *
  * @returns the homepage component
  */
 function Home(): JSX.Element {
