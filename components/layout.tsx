861a264ac44068a7f7731182404fa8352fddf8ee
March Sabino <marchsabino@gmail.com>
2021-03-09 16:38:22 -0500

    update styles

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,21 +11,20 @@ function Layout({ children, name }) {
           {process.env.NEXT_PUBLIC_SITE_NAME} - {name}
         </title>
       </Head>
-      <div className="flex flex-col h-screen">
-        <nav className="py-3 mb-5 bg-gray-700">
-          <div className="container flex justify-between text-md">
-            <span className="font-semibold text-gray-200">
-              {process.env.NEXT_PUBLIC_SITE_NAME}
-            </span>
-            <div id="social-media-links" className="text-gray-400">
-              <InstagramLink className="mr-2" />
-              <TwitterLink className="mr-2" />
-              <YoutubeLink />
-            </div>
+
+      <nav className="fixed w-full top-0 py-3 bg-gray-700">
+        <div className="container flex justify-between text-md">
+          <span className="font-semibold text-gray-200">
+            {process.env.NEXT_PUBLIC_SITE_NAME}
+          </span>
+          <div id="social-media-links" className="text-gray-400">
+            <InstagramLink className="mr-2" />
+            <TwitterLink className="mr-2" />
+            <YoutubeLink />
           </div>
-        </nav>
-        {children}
-      </div>
+        </div>
+      </nav>
+      <div className="mt-16">{children}</div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,54 @@ function Home(): JSX.Element {
   return (
     <Layout name="Home">
       <main>
-        <div className="container">
-          <div className="mx-auto max-w-2xl">
-            <div className="text-center text-green-800 bg-green-50 p-2 rounded border-green-200 border">
-              <strong>Notice:</strong> This website is currently growing 🌱
+        <div className="container max-w-screen-lg">
+          <div className="text-center text-green-800 bg-green-50 p-2 border-green-200 border">
+            <strong>Notice:</strong> This website is currently growing 🌱
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 my-5">
+            <img
+              className="max-h-96"
+              width="100%"
+              src="https://carlyriordan.com/wp-content/uploads/2021/01/Carly-Riordan-1-1.jpg"
+            />
+            <img
+              className="hidden sm:block"
+              src="https://carlyriordan.com/wp-content/uploads/2021/01/Carly-Riordan-3-scaled-1.jpg"
+            />
+            <img
+              className="hidden sm:block"
+              src="https://carlyriordan.com/wp-content/uploads/2021/01/Tuckernuck-Charlotte-Dress-1.jpg"
+            />
+          </div>
+
+          <div className="p-3 mb-10 border-2 border-gray-100">
+            <h1 className="text-lg font-semibold">Hello World! 👋</h1>
+            <div>
+              <img
+                height="16px"
+                width="16px"
+                className="rounded-full inline-block mr-1"
+                src="https://carlyriordan.com/wp-content/uploads/2021/01/Tuckernuck-Charlotte-Dress-1.jpg"
+              />
+              <span className="text-sm text-gray-400">
+                March 09, 2021 by Angela Sabino
+              </span>
             </div>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse ut convallis neque, ut laoreet magna. Pellentesque ut
+              leo orci. Praesent nec elit sapien. In rhoncus luctus sapien id
+              elementum. Nam elementum magna non nisi laoreet, fermentum rutrum
+              sapien finibus. Vivamus dictum non ante ac venenatis. Praesent
+              suscipit enim a leo laoreet porta. Mauris id lectus pharetra,
+              tincidunt mi ut, rutrum massa. Vestibulum suscipit posuere urna et
+              posuere. Fusce congue dolor interdum ligula elementum, quis
+              imperdiet dolor malesuada. Nulla pretium rutrum lacus, in dapibus
+              eros molestie eget. Suspendisse ornare lectus est, sit amet
+              posuere lorem suscipit id. Aliquam ut urna consequat, condimentum
+              mauris eget, interdum ex. Quisque augue lacus, egestas sed dapibus
+              vel, maximus et mauris. Morbi ut vulputate tortor.
+            </p>
           </div>
         </div>
       </main>
